1004f11f68a087888dfdfb809812dcf3ede68ac1
DmytroPopko <popkomu@gmail.com>
2022-09-08 23:44:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,58 +7,65 @@ import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 const refs = getRefs();
 
-refs.searchInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+function findCountryInf() {
+  refs.searchInput.addEventListener(
+    'input',
+    debounce(onSearch, DEBOUNCE_DELAY)
+  );
 
-function onSearch(e) {
-  clearCountryContainer();
-  const searchQuery = e.target.value.trim();
-  if (searchQuery === '') {
-    return;
+  function onSearch(e) {
+    clearCountryContainer();
+    const searchQuery = e.target.value.trim();
+    if (searchQuery === '') {
+      return;
+    }
+
+    API.fetchCountry(searchQuery).then(appendCountryMarkup);
   }
 
-  API.fetchCountry(searchQuery).then(appendCountryMarkup);
-}
+  function appendCountryMarkup(countries) {
+    console.log(countries.status);
+    if (countries.status === 404) {
+      onFetchError();
+    }
 
-function appendCountryMarkup(countries) {
-  console.log(countries.status);
-  if (countries.status === 404) {
-    onFetchError();
-  }
+    if (countries.length === 1) {
+      let languages = Object.values(countries[0].languages).join(', ');
 
-  if (countries.length === 1) {
-    let languages = Object.values(countries[0].languages).join(', ');
-
-    refs.countryContainer.insertAdjacentHTML(
-      'beforeend',
-      `<h1><img src="${countries[0].flags.svg}" alt="flag" width="30">&nbsp${countries[0].name.official}</h1>
+      refs.countryContainer.insertAdjacentHTML(
+        'beforeend',
+        `<h1><img src="${countries[0].flags.svg}" alt="flag" width="30">&nbsp${countries[0].name.official}</h1>
       <span>Capital: ${countries[0].capital}</span><br>
       <span>Population: ${countries[0].population}</span><br>
       <span>Languages: ${languages}</span>`
-    );
-  } else if (countries.length > 1 && countries.length <= 10) {
-    refs.countryContainer.insertAdjacentHTML(
-      'beforeend',
-      countries.reduce((acc, country) => {
-        acc += `<li><img src="${country.flags.svg}" alt="flag" width="30">
+      );
+    } else if (countries.length > 1 && countries.length <= 10) {
+      refs.countryContainer.insertAdjacentHTML(
+        'beforeend',
+        countries.reduce((acc, country) => {
+          acc += `<li><img src="${country.flags.svg}" alt="flag" width="30">
       &nbsp${country.name.official}</li>`;
-        return acc;
-      }, '')
+          return acc;
+        }, '')
+      );
+    } else if (countries.length > 10) {
+      toManyMatches();
+    }
+  }
+
+  function onFetchError() {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+  }
+
+  function toManyMatches() {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
     );
-  } else if (countries.length > 10) {
-    toManyMatches();
+  }
+
+  function clearCountryContainer() {
+    refs.countryContainer.innerHTML = '';
   }
 }
 
-function onFetchError() {
-  Notiflix.Notify.failure('Oops, there is no country with that name');
-}
-
-function toManyMatches() {
-  Notiflix.Notify.info(
-    'Too many matches found. Please enter a more specific name.'
-  );
-}
-
-function clearCountryContainer() {
-  refs.countryContainer.innerHTML = '';
-}
+findCountryInf();
